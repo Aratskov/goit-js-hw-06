85dc4aae183770bd2c9a77980e3257a59b8f9842
Aratskov <ifromukraine2022@gmail.com>
2023-02-18 10:24:46 +0200

    10 исправил

--- js/task-10.js
+++ js/task-10.js
@@ -1,37 +1,35 @@
-const createEl = document.querySelector('[data-create]');
-const destroyEl = document.querySelector('[data-destroy]');
-const boxesEl = document.querySelector('#boxes');
-const inputEl = document.querySelector('input');
+const createEl = document.querySelector("[data-create]");
+const destroyEl = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+const inputEl = document.querySelector("input");
 
-createEl.addEventListener('click', createBoxes);
-inputEl.addEventListener('blur', inputNumber);
-destroyEl.addEventListener('click',destroyBoxes);
+createEl.addEventListener("click", createBoxes);
+inputEl.addEventListener("blur", inputNumber);
+destroyEl.addEventListener("click", destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 let number = 0;
 
-function inputNumber(event){
+function inputNumber(event) {
   number = event.currentTarget.value;
 }
 
-
-function createBoxes(amount){
+function createBoxes(amount) {
   amount = number;
   let total = 30;
-  
-  for(let i = 0; i < amount; i++){
-    const div = document.createElement('div');
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
     boxesEl.append(div);
     div.style.width = `${total}px`;
     div.style.height = `${total}px`;
     total += 10;
-    div.style.backgroundColor = `${getRandomHexColor()}`
+    div.style.backgroundColor = `${getRandomHexColor()}`;
   }
 }
 
-
-function destroyBoxes(event){
-  boxesEl.innerHTML ="";
+function destroyBoxes(event) {
+  boxesEl.innerHTML = "";
 }
