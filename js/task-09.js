142eec86ad19f20d2fa8d535f9bbcd81f5665192
Aratskov <ifromukraine2022@gmail.com>
2023-02-18 10:23:56 +0200

    9 исправил

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,15 @@
-const body = document.querySelector('body');
-const button = document.querySelector('.change-color');
-const span = document.querySelector('.color')
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
 
-button.addEventListener('click', randomColor)
+button.addEventListener("click", randomColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function randomColor(event){
-  body.style.backgroundColor = `${getRandomHexColor()}`;
-  span.textContent = `${getRandomHexColor()}`
+function randomColor(event) {
+  const color = getRandomHexColor()
+  body.style.backgroundColor = color;
+  span.textContent = color;
 }
-
